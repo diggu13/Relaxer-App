0d63e25b9cc2ceec1a094d00e3ad4caa3bd7e04c
virender <virender@fanaticcoders.com>
2022-02-11 11:36:42 +0530

    added files

---
+++ app.js
@@ -0,0 +1,20 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+function breathAnimation(){
+    text.innerHTML='Breathe In !'
+    container.className = 'container grow'
+    setTimeout(()=>{
+        text.innerHTML='Hold'
+        setTimeout(()=>{
+            text.innerHTML = 'Breathe Out';
+            container.className='container shrink'
+        },holdTime);
+    },breathTime)
+}
+breathAnimation();
+setInterval(breathAnimation , totalTime)
